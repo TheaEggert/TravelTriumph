ea41436293ccd98920a8a61738e7f516db4cfad6
Thea <eggert@ualberta.ca>
2023-02-01 17:34:48 -0700

    Changed City name field from UUID to Char

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -2,9 +2,9 @@ from django.db import models
 import uuid
 
 class City(models.Model):
-    name = models.UUIDField(primary_key=True, auto_created=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(primary_key=True, max_length=100)
     label = models.CharField(max_length=60)
-    description = models.CharField(max_length=60)
+    description = models.CharField(max_length=100)
 
     def __str__(self):
         return self.label
