5666dddac05d645ba43c4c43ee1a0d0550a06ed5
Thea <eggert@ualberta.ca>
2023-02-04 12:27:45 -0700

    Increased max description length

--- backend/backend/models.py
+++ backend/backend/models.py
@@ -4,7 +4,7 @@ import uuid
 class City(models.Model):
     name = models.CharField(primary_key=True, max_length=100)
     label = models.CharField(max_length=60)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
     latitude = models.DecimalField(max_digits=4, decimal_places=2, default=0)
     longitude = models.DecimalField(max_digits=5, decimal_places=2, default=0)
 
