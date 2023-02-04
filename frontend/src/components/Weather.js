6f1a794adeb04d8cf678938edf60d08316054e9c
Thea <eggert@ualberta.ca>
2023-02-04 11:10:49 -0700

    Round temperature values

--- frontend/src/components/Weather.js
+++ frontend/src/components/Weather.js
@@ -5,6 +5,9 @@ import React, { useState } from 'react';
 export default function Weather({latitude, longitude}) {
 
     const [conditions, setConditions] = useState([]);
+    const [temp, setTemp] = useState([]);
+    const [hi, setHi] = useState([]);
+    const [lo, setLo] = useState([]);
     const apiKey = process.env.REACT_APP_API_KEY;
     const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
@@ -12,6 +15,9 @@ export default function Weather({latitude, longitude}) {
     const fetchData = React.useCallback(async () => {
         const result = await axios(apiURL);
         setConditions(result.data.weather[0].description);
+        setTemp(Math.round(result.data.main.temp));
+        setLo(Math.round(result.data.main.temp_min));
+        setHi(Math.round(result.data.main.temp_max));
     }, [apiURL]);
 
     React.useEffect(() => {
@@ -21,7 +27,20 @@ export default function Weather({latitude, longitude}) {
     return(
         <div>
             <div style={{ color: "GrayText", marginTop: "30px" }}>Weather</div>
-            {"weather is: " + conditions}
+            <div>
+                <br></br>
+                {conditions}
+            </div>
+            <div style={{ fontSize: "35px" }}>
+                {temp}&deg;C
+            </div>
+            <br></br>
+            <div>
+                {"L:" + lo}&deg;C
+            </div>
+            <div>
+                {"H:" + hi}&deg;C
+            </div>
         </div>
     );
 }
