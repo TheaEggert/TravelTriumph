cdbd889bd9b23a6fe059931a6270dd89c0c67b0c
Thea <eggert@ualberta.ca>
2023-02-06 00:25:39 -0700

    cleaned up Selection.js

--- frontend/src/components/Selection.js
+++ frontend/src/components/Selection.js
@@ -1,13 +1,8 @@
-// Refactor this file
-
 import React, { useState } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import '../Selection.css';
-import { styled } from '@mui/material/styles';
-import InputBase from '@mui/material/InputBase';
 import { getCities } from '../service';
 
 export default function Selection({setAppDescription, setCitySelected, setLatitude, setLongitude}) {
@@ -28,48 +23,26 @@ export default function Selection({setAppDescription, setCitySelected, setLatitu
 
   const handleChange = (event) => {
 
-    for (let i = 0; i < cityList.length; i += 1) {
-      if (cityList[i].label === event.target.value) {
-        setAppDescription(cityList[i].description);
-        setLatitude(cityList[i].latitude);
-        setLongitude(cityList[i].longitude);
+    for (const element of cityList) {
+      if (element.label === event.target.value) {
+        setAppDescription(element.description);
+        setLatitude(element.latitude);
+        setLongitude(element.longitude);
       }
     }
     setSelectedCity(event.target.value);
     setCitySelected(true);
   };
 
-  // todo: refactor (see MUI 'Selection' webpage)
-  const BootstrapInput = styled(InputBase)(({ theme }) => ({
-    'label + &': {
-      marginTop: theme.spacing(0.7),
-    },
-    '& .MuiInputBase-input': {
-      borderRadius: 4,
-      position: 'relative',
-      backgroundColor: theme.palette.background.paper,
-      border: '1px solid #ced4da',
-      fontSize: 16,
-      padding: '10px 26px 10px 12px',
-      transition: theme.transitions.create(['border-color', 'box-shadow']),
-      '&:focus': {
-        borderRadius: 4,
-        borderColor: '#80bdff',
-        boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-      },
-    },
-  }));
-
   return (
-    <div className="selection-bg">
-      <div className="selection">
+      <div>
         <FormControl sx={{ m: 1, width: '45%' }}>
           <InputLabel id="selection-label">Select a city</InputLabel>
           <Select
             id='selection-bar'
             value={selectedCity}
             onChange={handleChange}
-            input={<BootstrapInput />}
+            label="Select a city"
           >
             {cityList.map((city) => (
               <MenuItem key={city.name} value={city.label}>{city.label}</MenuItem>
@@ -77,6 +50,5 @@ export default function Selection({setAppDescription, setCitySelected, setLatitu
           </Select>
         </FormControl>
       </div>
-    </div>
   );
 }
